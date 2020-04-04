af214fcf2de0f5046dfa57bdde08c7ee0a6d9faf
Vini Dalvino <vinidalvino@cock.li>
2020-04-03 21:00:47 -0400

    made the snippet as global var to avoid a stack overflow

--- streght_api.lua
+++ streght_api.lua
@@ -12,7 +12,7 @@ local ReplicatedFirst = game:GetService('ReplicatedFirst')
 local Backpack = LocalPlayer.Backpack
 local Character = LocalPlayer.Character
 local Humanoid = Character.Humanoid
-local HumanoidRootFrame = Character.HumanoidRootPart 
+local HumanoidRootPart = Character.HumanoidRootPart 
 -- return an array that indicate which training device are used and which are not used
 getgenv().strenghtAuto = {}
 getgenv().strenghtAuto.timeItRepeat = 1 -- the default step of cycle of the auto farm
@@ -25,10 +25,10 @@ end
 
 isStrenghtToolUsed = function() 
     local trainingDevice = {
-        Crunches    = workspace.Crunches.In_Use.Value;
-        Leg_Lift    =  workspace.Leg_Lift.In_Use.Value;
-        Squat_Jumps = workspace.Squat_Jumps.In_Use.Value;
-        Push_Ups    = workspace.Push_Ups.In_Use.Value;
+        ["workspace.Crunches"]    = workspace.Crunches.In_Use.Value;
+        ['workspace.Leg_Lift']    =  workspace.Leg_Lift.In_Use.Value;
+        ['workspace.Squat_Jumps'] = workspace.Squat_Jumps.In_Use.Value;
+        ['workspace.Push_Ups']    = workspace.Push_Ups.In_Use.Value;
     }
     return trainingDevice
 end
@@ -43,5 +43,24 @@ setAmountItWillRepeat = function(timeItRepeat)
 end
 
 doStreghtAutoFarm = function()
-    
+    local trainingDevice = {
+        ["workspace.Crunches"]     = {workspace.Crunches.In_Use.Value; CFrame3.new(-142.26973, 3.69035339, -64.4220047)};
+        ['workspace.Leg_Lift']     = {workspace.Leg_Lift.In_Use.Value;CFrame3.new(-127.155914, 3.69005132, -65.509613)};
+        ['workspace.Squat_Jumps']  = {workspace.Squat_Jumps.In_Use.Value;CFrame3.new(-113.031052, 3.69005132, -65.5107956)};
+        ['workspace.Push_Ups']     = {workspace.Push_Ups.In_Use.Value;CFrame3.new(-97.0513, 3.69003153, -66.1871414)};
+        ['workspace.Overhead1']    = {workspace.Overhead1.In_Use.Value;CFrame3.new(-95.6423416, 3.50129175, 63.8929062)};
+        ['workspace.Bicep1']       = {workspace.Bicep1.In_Use.Value;CFrame3.new(-108.193413, 3.50129175, 60.8449173)};
+        ['workspace.Squat1']       = {workspace.Squat1.In_Use.Value;CFrame3.new(-118.741066, 3.50129128, 61.4322205)};
+        ['workspace.Bench1']       = {workspace.Bench1.In_Use.Value;CFrame3.new(-130.319031, 3.44129109, 57.6213875)};
+    }
+    local toolToFarmOn
+    for i,v in pairs( isStrenghtToolUsed() ) do
+        if v == true then
+            toolToFarmOn = v
+            break
+        end
+    end
+    print(toolToFarmOn)
 end
+
+doStreghtAutoFarm()
