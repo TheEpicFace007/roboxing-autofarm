a1b0f5db3a43f8b80dc0eccbe5c4ef7279f74c25
Vini Dalvino <vinidalvino@cock.li>
2020-04-05 20:35:14 -0400

    FINALLY MADE THE AUTO FARM PART WHERE IT CLICK WORKING[FN : doSteghtAutoDarm

--- streght_api.lua
+++ streght_api.lua
@@ -78,25 +78,26 @@ pathfind = function(Position,isShowingBreadcrumb)
     end
 end
 
+local blankExerciseLabel = trainingGui.Exercise_Prompt.Exercise_Name
 doStreghtAutoFarm = function()
     for k = 1,strenghtAuto.timeItRepeat do
         repeat wait() until Humanoid.WalkSpeed == 16
         trainingDevice = {
-       Crunches     = {workspace.Crunches.In_Use.Value   ;Vector3.new(-142.26973, 3.69035339, -64.4220047)};
-       Leg_Lift     = {workspace.Leg_Lift.In_Use.Value   ;Vector3.new(-127.155914, 3.69005132, -65.509613)};
-       Squat_Jumps  = {workspace.Squat_Jumps.In_Use.Value;Vector3.new(-113.031052, 3.69005132, -65.5107956)};
-       Push_Ups     = {workspace.Push_Ups.In_Use.Value   ;Vector3.new(-97.0513, 3.69003153, -66.1871414)};
-       Overhead1    = {workspace.Overhead1.In_Use.Value  ;Vector3.new(-95.6423416, 3.50129175, 63.8929062)};
-       Bicep1       = {workspace.Bicep1.In_Use.Value     ;Vector3.new(-108.193413, 3.50129175, 60.8449173)};
-       Squat1       = {workspace.Squat1.In_Use.Value     ;Vector3.new(-118.741066, 3.50129128, 61.4322205)};
-       Bench1       = {workspace.Bench1.In_Use.Value     ;Vector3.new(-130.319031, 3.44129109, 57.6213875)};
+            Crunches     = {workspace.Crunches.In_Use.Value   ;Vector3.new(-142.26973, 3.69035339, -64.4220047)};
+            Leg_Lift     = {workspace.Leg_Lift.In_Use.Value   ;Vector3.new(-127.155914, 3.69005132, -65.509613)};
+            Squat_Jumps  = {workspace.Squat_Jumps.In_Use.Value;Vector3.new(-113.031052, 3.69005132, -65.5107956)};
+            Push_Ups     = {workspace.Push_Ups.In_Use.Value   ;Vector3.new(-97.0513, 3.69003153, -66.1871414)};
+            Overhead1    = {workspace.Overhead1.In_Use.Value  ;Vector3.new(-95.6423416, 3.50129175, 63.8929062)};
+            Bicep1       = {workspace.Bicep1.In_Use.Value     ;Vector3.new(-108.193413, 3.50129175, 60.8449173)};
+            Squat1       = {workspace.Squat1.In_Use.Value     ;Vector3.new(-118.741066, 3.50129128, 61.4322205)};
+            Bench1       = {workspace.Bench1.In_Use.Value     ;Vector3.new(-130.319031, 3.44129109, 57.6213875)};
         }
         local toolToFarmOn
         for i,v in pairs( trainingDevice ) do
-        if v[1] == false then
-            toolToFarmOn = v
-            break
-        end
+            if v[1] == false then
+                toolToFarmOn = v
+                break
+            end
         end
         onPathBlocked = function(blockedWaypointIndex)
         if blockedWaypointIndex > currentWaypointIndex then
@@ -107,8 +108,9 @@ doStreghtAutoFarm = function()
         end
         end
         pathfind(toolToFarmOn[2],true)
-        repeat wait() until trainingGui.Exercise_Prompt.Visible == true
-        fireMb1Click()
+        repeat wait()
+        until trainingGui.Exercise_Prompt.Exercise_Name.Value ~= ""
+        emulateBtnClick(trainingGui.Exercise_Prompt)
     end
 end
 
